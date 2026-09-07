09c5d706fcc901e32319d6525a641f1be5022447
Aasif Khan <aasifkhan.a2006@gmail.com>
2026-09-07 18:28:49 +0530

    fix: bug in logger

--- backend/src/config/logger.js
+++ backend/src/config/logger.js
@@ -1,12 +1,13 @@
 import pino from "pino";
 
-const logger = process.env.NODE_ENV
-  ? pino({
-      level: "info",
-      transport: {
-        target: "pino-pretty",
-      },
-    })
-  : pino();
+const logger =
+  process.env.NODE_ENV == "development"
+    ? pino({
+        level: "info",
+        transport: {
+          target: "pino-pretty",
+        },
+      })
+    : pino();
 
 export { logger };
